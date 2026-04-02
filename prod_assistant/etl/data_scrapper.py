d5e582be3ed0eaf8490e638424f2599adeee2e58
Madhushivaganga KS <madhushivagangaks@gmail.com>
2026-04-02 21:04:26 +0530

    Static folders updated

--- prod_assistant/etl/data_scrapper.py
+++ prod_assistant/etl/data_scrapper.py
@@ -19,7 +19,7 @@ class FlipkartScraper:
         options = uc.ChromeOptions()
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-blink-features=AutomationControlled")
-        driver = uc.Chrome(options=options,use_subprocess=True)
+        driver = uc.Chrome(options=options,use_subprocess=True,version_main=146)
 
         if not product_url.startswith("http"):
             driver.quit()
@@ -60,7 +60,7 @@ class FlipkartScraper:
         """Scrape Flipkart products based on a search query.
         """
         options = uc.ChromeOptions()
-        driver = uc.Chrome(options=options,use_subprocess=True)
+        driver = uc.Chrome(options=options,use_subprocess=True,version_main=146)
         search_url = f"https://www.flipkart.com/search?q={query.replace(' ', '+')}"
         driver.get(search_url)
         time.sleep(4)
